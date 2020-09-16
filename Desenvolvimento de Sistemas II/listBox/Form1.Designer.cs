fb1dfd79890a914739b43c3488d3c4a584f170fc
Eduardo Rogério <eduardorogerio@protonmail.com>
2020-09-15 23:15:12 -0300

    Corrigido o exercicio listBox

--- Desenvolvimento de Sistemas II/listBox/Form1.Designer.cs
+++ Desenvolvimento de Sistemas II/listBox/Form1.Designer.cs
@@ -70,6 +70,7 @@
             this.listBox1.ItemHeight = 17;
             this.listBox1.Location = new System.Drawing.Point(21, 56);
             this.listBox1.Name = "listBox1";
+            this.listBox1.SelectionMode = System.Windows.Forms.SelectionMode.MultiExtended;
             this.listBox1.Size = new System.Drawing.Size(427, 259);
             this.listBox1.TabIndex = 2;
             this.listBox1.SelectedIndexChanged += new System.EventHandler(this.listBox1_SelectedIndexChanged);
